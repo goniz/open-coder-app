7d5a8cdea502869e011dbb75b81981be3e2239f9
Goni Zahavy <goni1993@gmail.com>
2025-09-08 08:55:17 +0300

    Initial commit

---
+++ Sources/Views/AppView.swift
@@ -0,0 +1,29 @@
+import ComposableArchitecture
+import Features
+import SwiftUI
+
+package struct AppView: View {
+  let store: StoreOf<AppFeature>
+
+  package init(store: StoreOf<AppFeature>) {
+    self.store = store
+  }
+
+  package var body: some View {
+    HomeView(store: store.scope(state: \.home, action: \.home))
+      .task {
+        await store.send(.task).finish()
+      }
+  }
+}
+
+#Preview {
+  AppView(
+    store: .init(
+      initialState: .init(),
+      reducer: {
+        AppFeature()
+      }
+    )
+  )
+}
---
+++ Sources/Views/HomeView.swift
@@ -0,0 +1,29 @@
+import ComposableArchitecture
+import Features
+import SwiftUI
+
+struct HomeView: View {
+  let store: StoreOf<HomeFeature>
+
+  init(store: StoreOf<HomeFeature>) {
+    self.store = store
+  }
+
+  var body: some View {
+    Text("Home", bundle: .module)
+      .task {
+        await store.send(.task).finish()
+      }
+  }
+}
+
+#Preview {
+  HomeView(
+    store: .init(
+      initialState: .init(),
+      reducer: {
+        HomeFeature()
+      }
+    )
+  )
+}
